dc7a5ee332f600649b6c576715d1dc6821dc3678
Nick Jeske <nickjeske49@gmail.com>
2019-12-04 20:46:42 -0600

    Update Database

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '',
-    database : 'facial-recognition-app'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   },
-  pool: { min: 0, max: 7 }
 });
 
 
